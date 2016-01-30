3d8615a8773d88820ea9335e4f90da7109fdc007
Sonia <s49hu@uwaterloo.ca>
2016-01-30 16:48:40 -0500

    fixed courseList

--- coursecheck.py
+++ coursecheck.py
@@ -8,13 +8,14 @@ import ctypes
 
 ## Constants
 url = 'https://api.uwaterloo.ca/v2/courses'
-gTerm = 1159      #The number for the desired term
+gTerm = 1161      #The number for the desired term
 gKey = '063082f318c211babaf6c579c69c3323'  #the API key
 minpertry = 20    #number of minutes between attempts;
                   #note that the API key has a use limit of 25000 per month
 secs_per_min = 60;
 tries = 0;  #number of attemps made so far
 success = False;  #set to true when any course is open
+courseList = [6127]; #list of courses to look for
 
 # api search takes in sCourse, an integer,
 # sTerm, a 4-digit integer, and sKey, an API key,
@@ -38,7 +39,7 @@ def apiSearch(sCourse,sTerm,sKey):
     # success: course is open
     if enrolTot < enrolCap:
         text = ''
-        text += 'Class : ' + course + '\n'
+        text += 'Class : ' + coursetitle + '\n'
         text += 'Enrolment Cap : ' + str(enrolCap) + '\n'
         text += 'Enrolment Total : ' + str(enrolTot) + '\n'
         text += 'Instructor : ' + str(instructor)
@@ -53,7 +54,7 @@ while True:
     tries += 1;
     print "Tries : " + str(tries)
     for i in range(0, len(courseList)):
-        apiSearch(courselist[i],gTerm,gKey);
+        apiSearch(courseList[i],gTerm,gKey);
     if (success == True):
         break;
     else:
